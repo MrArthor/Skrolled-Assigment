1c5daf89adbf87ce70aba0b645a8daeea9a02715
Vansh Sachdev <vksvs25@gmail.com>
2023-09-29 03:02:28 +0530

    Update

--- Seed/index.js
+++ Seed/index.js
@@ -3,21 +3,20 @@ const Data = require("./MOCK_DATA.js");
 const ProductModel = require("../Model/Product");
 mongoose.set("strictQuery", false);
 
-const URL =  "mongodb+srv://qwert:vanshsachdeva@cluster0.7zuhob5.mongodb.net/PlotLine?retryWrites=true&w=majority";
+const URL = "mongodb+srv://qwert:vanshsachdeva@cluster0.7zuhob5.mongodb.net/PlotLine?retryWrites=true&w=majority";
 
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(URL,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+const connectDB = async() => {
+    try {
+        await mongoose.connect(URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
         });
 
-          console.log("Database connected");
-  } catch (error) {
-    console.log("error", error);
-  }
+        console.log("Database connected");
+    } catch (error) {
+        console.log("error", error);
+    }
 };
 connectDB();
 
@@ -25,32 +24,36 @@ const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-  console.log("Database connected");
+    console.log("Database connected");
 });
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
-const seedDB = async () => {
-  await ProductModel.deleteMany({});
-  mongoose.set("strictQuery", true);
+const seedDB = async() => {
+    await ProductModel.deleteMany({});
+    mongoose.set("strictQuery", true);
 
-  for (let i = 0; i < 10; i++) {
-    const product = new ProductModel({
-      name: Data[i].productname,
-      type: 'Product',
-      price: Data[i].price,
-    });
-    await product.save();
-  }for (let i = 10; i < 20; i++) {
-    const product = new ProductModel({
-      name: Data[i].productname,
-      type: 'service',
-      price: Data[i].price,
-    });
-    await product.save();
-  }
+    for (let i = 0; i < 10; i++) {
+        const product = new ProductModel({
+            name: Data[i].product_name,
+            image: 'https://m.economictimes.com/thumb/msid-100966456,width-1200,height-900,resizemode-4,imgsize-63314/why-become-a-product-manager.jpg',
+            type: 'Product',
+            category: 'Data[i].category',
+
+            price: Data[i].price,
+        });
+        await product.save();
+    }
+    for (let i = 10; i < 20; i++) {
+        const product = new ProductModel({
+            name: Data[i].productname,
+            type: 'service',
+            price: Data[i].price,
+        });
+        await product.save();
+    }
 };
 
 seedDB().then(() => {
-  mongoose.connection.close();
+    mongoose.connection.close();
 });
